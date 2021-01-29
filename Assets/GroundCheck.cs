5c5b6471b55ecf282c6620a5da05753cb5e1303d
Quint <quint@thespace.social>
2021-01-29 22:49:46 +0100

    Added smart invertion

--- Assets/GroundCheck.cs
+++ Assets/GroundCheck.cs
@@ -8,10 +8,18 @@ public class GroundCheck : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Vector3 closestPoint = collision.bounds.ClosestPoint(playerCtrl.rb2d.position);
+        if (playerCtrl.rb2d.position.x > closestPoint.x)
+        {
+            playerCtrl.inverted = true;
+        } else if (playerCtrl.rb2d.position.y > closestPoint.y) {
+            playerCtrl.inverted = true;
+        }
         playerCtrl.grounded = true;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         playerCtrl.grounded = false;
+        playerCtrl.inverted = false;
     }
 }
--- Assets/PlayerCrtl.cs
+++ Assets/PlayerCrtl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerCrtl : MonoBehaviour
 {
     public bool grounded;
+    public bool inverted;
     public Rigidbody2D rb2d;
     public float bounceStrength;
     // Start is called before the first frame update
@@ -19,20 +20,9 @@ public class PlayerCrtl : MonoBehaviour
        
         if (grounded)
         {
-            /*
-            Vector2 position = new Vector2(transform.position.x, transform.position.y);
-            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector2 direction = position + mousePosition;
-            Vector2 newvector = (direction * bounceStrength) /10;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePosition, 1f);
-            Debug.DrawRay(transform.position, Vector2.right, Color.green, 1f);
-            Vector2 rayLoc = hit.point;
-            rb2d.velocity = rayLoc *-1;
-            Debug.Log(hit.point);
-            //Debug.Log("Mouse Pos: " + mousePosition + " Direction: " + direction + " New Vector: " + newvector);*/
             Vector2 thisPos = transform.position;
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (mousePosition.y < 0)
+            if (inverted)
             {
                 mousePosition.y = mousePosition.y * -1;
             }
