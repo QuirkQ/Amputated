333d1d9caa3c651000ac9a5b4732870f1acc26c8
guuzz131 <guuzz@icloud.com>
2021-01-29 22:23:48 +0100

    Character Versie 1

--- Assets/PlayerCrtl.cs
+++ Assets/PlayerCrtl.cs
@@ -19,13 +19,26 @@ public class PlayerCrtl : MonoBehaviour
        
         if (grounded)
         {
-            
+            /*
             Vector2 position = new Vector2(transform.position.x, transform.position.y);
             Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             Vector2 direction = position + mousePosition;
-            Vector2 newvector = (direction * bounceStrength) * -1/10;
-            rb2d.velocity = newvector;
-            Debug.Log("Mouse Pos: " + mousePosition + " Direction: " + direction + " New Vector: " + newvector);
+            Vector2 newvector = (direction * bounceStrength) /10;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePosition, 1f);
+            Debug.DrawRay(transform.position, Vector2.right, Color.green, 1f);
+            Vector2 rayLoc = hit.point;
+            rb2d.velocity = rayLoc *-1;
+            Debug.Log(hit.point);
+            //Debug.Log("Mouse Pos: " + mousePosition + " Direction: " + direction + " New Vector: " + newvector);*/
+            Vector2 thisPos = transform.position;
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (mousePosition.y < 0)
+            {
+                mousePosition.y = mousePosition.y * -1;
+            }
+            Vector2 direction = mousePosition - thisPos;
+            transform.right = direction;
+            rb2d.velocity = transform.right * bounceStrength;
         }
     }
 }
