0a05380cfb3aa705716206e876f2087ca70477c2
Joost Grijpma <joostgrijpma18@gmail.com>
2021-01-30 11:22:54 +0100

    UI
    AddedTMP
    Simple main menu
    other scenes

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement: MonoBehaviour
+{
+    public void SceneMainMenu()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void SceneYouWon()
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SceneFirstLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void levelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+}
